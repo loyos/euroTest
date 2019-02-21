cf48c8cf85cf0e4d8917ebec56a018deb37ab1fc
unknown <loyenrique1@gmail.com>
2019-02-21 18:02:02 +0100

    folder structure and simple html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { EurovisionModule } from './eurovision/eurovision.module';
 
 import { AppComponent } from './app.component';
 
+const appRoutes: Routes = [
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
+    BrowserModule,
+    EurovisionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
